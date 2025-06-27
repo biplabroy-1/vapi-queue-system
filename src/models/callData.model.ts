01b59b3f6279b1a8f3c8aa77b78abd219aede309
Biplab Roy <biplabr119201@gmail.com>
2025-06-28 04:47:53 +0530

    fixed

--- src/models/callData.model.ts
+++ src/models/callData.model.ts
@@ -2,6 +2,9 @@ import mongoose, { Schema } from "mongoose";
 
 const CallDataSchema = new Schema({}, { strict: false });
 
+CallDataSchema.index({ userId: 1 });
+CallDataSchema.index({ startedAt: 1 });
+
 // export default mongoose.model("CallData", CallDataSchema); 
 const CallData = mongoose.models.CallData || mongoose.model("CallData", CallDataSchema);
 export default CallData
